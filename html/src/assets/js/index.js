6b2315fccdc4107fdf69b5dd0af6ca1198a991d4
maeshun <butterflife2007@gmail.com>
2016-10-16 03:20:52 +0900

    動画ある程度実装

--- html/src/assets/js/index.js
+++ html/src/assets/js/index.js
@@ -10,8 +10,34 @@
 
     if (!isSmartDevice()) {
       console.log("pc");
-      // appendOpeningMovie();
+      appendOpeningMovie();
+    } else {
+      $(".video").css({
+        "background-image": "url(/assets/img/sp_dummy_top.jpg)",
+        "background-size" : "cover",
+        "background-repeat" : "no-repeat",
+        "background-position" : "center center"
+      });
+      $(".openingMask").fadeOut(2000);
     }
+
+
+    $(".videoOver__down").on("click", function(){
+      $(".video").animate({
+        "top": "-100%",
+        "opacity": "0.5"
+      }, 500, "linear", function(){
+        console.log("れもゔぇ")
+        $("body").removeClass("openingVideo");
+        $(".openingContent").remove();
+      });
+      $("video").animate({
+        "top": "-50%",
+        "opacity": "0.5"
+      }, 500, "linear");
+      $(".openingLogo").fadeOut();
+      // $("body").removeClass("openingVideo");
+    })
 
 		// $(".footerLink__list a, .rimNavi__list a").on("click", function(){
 
@@ -31,14 +57,40 @@
     "init" : function() {
       var me = this;
       $(".header__menuBtn").on("click", function(){
-        $(this).toggleClass("header__menuBtn--open");
-        me.open();
+        if ($(this).hasClass("header__menuBtn--open")) {
+          // $(this).toggleClass("header__menuBtn--open");
+          me.close();
+        } else {
+          // $(this).toggleClass("header__menuBtn--open");
+          me.open();          
+        }
+      })
+
+
+      $(".navi a").on("click", function() {
+        var targetHash = $(this).attr("href");
+        me.scroll(targetHash);
       })
     },
     "open" : function() {
-      // $(".header__navi").toggleClass("header__navi--open");
-      $(".header__navi").fadeIn();
+      $(".header__menuBtn").addClass("header__menuBtn--open");
+      $(".header__navi").fadeIn(200);
+      // $(".header__navi").addClass("header__navi--open");
       console.log("open")
+    },
+    "close" : function() {
+      $(".header__menuBtn").removeClass("header__menuBtn--open");
+      $(".header__navi").fadeOut(200);
+      // $(".header__navi").removeClass("header__navi--open");
+      console.log("close")      
+    },
+    "scroll" : function(targetHash) {
+      var me = this;
+      $("html,body").animate({scrollTop: $(targetHash).offset().top}, function() {
+        setTimeout(function(){
+          me.close()
+        }, 200);
+      });
     }
   }
 
@@ -46,7 +98,7 @@
   function appendOpeningMovie() {
     var openingMovieDom = "";
     openingMovieDom += '<video src="assets/movie/movie24fps640.mp4" type="video/mp4" autoplay loop></video>';
-    openingMovieDom += '<div class="video__cover"></div>';
+    // openingMovieDom += '<div class="video__cover"><div class="videoOver__down"><a href="javascript:void(0);">CONTENT</a></div></div>';
     $(".video").append(openingMovieDom);
     $("video").ready(function(){
       $(".openingMask").fadeOut(2000);
